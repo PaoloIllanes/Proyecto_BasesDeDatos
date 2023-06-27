c7f0ac71286051bfe3237435f9cb627422ad16e1
PaoloIllanes <oeuthfoierughi@gmail.com>
2023-06-26 23:43:31 -0400

    data

--- scripts/3DB_indexing.sql
+++
@@ -1,7 +0,0 @@
-USE Farmacia;
-CREATE INDEX idx_precio_medicamentos ON medicamentos (precio);
-CREATE INDEX idx_proveedor_fecha_pedidos ON pedidos (id_proveedor, fecha);
-CREATE INDEX idx_stock_medicamentos ON medicamentos (stock);
-CREATE INDEX idx_fecha_ventas ON ventas(fecha);
-
-
